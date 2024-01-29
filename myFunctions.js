5f087cab347001bd15f22d3749fe9e0a53f764a3
Jorge Paul <jserranopaul@gmail.com>
2024-01-29 20:49:03 +0100

    Now they work

--- myFunctions.js
+++ myFunctions.js
@@ -1,17 +1,11 @@
 // ES5
 
-export function sayHello1() {
-  /**
-   * Take a paramenter called name and return a string that
-   * e.g. "Hello, John"
-   * */
+export function sayHello1(name) {
+  return `Hello, ${name}`;
 }
 
 // ES6
 
-export const sayHello2 = () => {
-  /**
-   * Take two paramenter one name and second one called timeOfDay (morning, evening, night) return a string that greets the name at that time of day
-   * e.g. with name="John" and timeOfDay="morning", return "Good morning, John". The COMMA IS IMPORTANT!
-   * */
+export const sayHello2 = (name, timeOfDay) => {
+  return `Good ${timeOfDay}, ${name}`;
 };
